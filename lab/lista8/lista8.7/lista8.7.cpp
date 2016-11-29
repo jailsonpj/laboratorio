7fff247acd13b3fb9ae4764086e81c4f770bd344
jailsonpj <jailsoncoloares@gmail.com>
2016-11-28 21:48:47 -0400

    up

--- lab/lista8/lista8.7/lista8.7.cpp
+++ lab/lista8/lista8.7/lista8.7.cpp
@@ -1,2 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "lista8.7.h"
+#define tam 2
+
+int main(int argc, char const *argv[])
+{
+	
+
+	for (int i = 0; i < tam; ++i)
+	{
+		printf("código dos cursos:\n");
+		printf("1-Engenharia\n");
+		printf("2-Computação\n");
+		printf("3-Administração\n");
+		scanf("%d",alu[i].codigo);
+		printf("Digite idade: ");
+		scanf("%d",alu[i].idade);
+	}
+
+	numero_alunos(alu);
+	printf("****************\n");
+	qnt_idade(alu);
+	printf("****************\n");
+	
+	return 0;
+}
--- lab/lista8/lista8.7/lista8.7.h
+++ lab/lista8/lista8.7/lista8.7.h
@@ -1,2 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define tam 2
+
+typedef struct aluno Aluno;
+struct aluno
+{
+	int codigo;
+	int idade;
+};
+
+Aluno alu[tam];
+
+
+void numero_alunos(Aluno *aux)
+{
+
+	int qnt1=0,qnt2=0,qnt3=0;
+
+	for (int i = 0; i < tam; ++i)
+	{
+		if (aux[i].codigo == 1)
+		{
+			qnt1 = qnt1 + 1;
+		}
+		if (aux[i].codigo == 2)
+		{
+			qnt2 = qnt2 + 1;
+		}
+		if (aux[i].codigo == 3)
+		{
+			qnt3 = qnt3 + 1;
+		}
+	}
+
+	printf("A quantidade de alunos de Engenharia: %d\n",qnt1 );
+	printf("A quantidade de alunos de Computação: %d\n",qnt2 );
+	printf("A quantidade de alunos de Administração: %d\n",qnt3 );
+
+}
+
+void qnt_idade (Aluno *aux)
+{
+	int qnt1=0,qnt2=0,qnt3=0;
+
+	for (int i = 0; i < tam; ++i)
+	{
+		if (aux[i].codigo == 1)
+		{
+			if (aux[i].idade >= 20 && aux[i].idade <= 28)
+			{
+				qnt1 = qnt1 + 1;
+			}
+			
+		}
+		if (aux[i].codigo == 2)
+		{
+			if (aux[i].idade >= 20 && aux[i].idade <= 28)
+			{
+				qnt2 = qnt2 + 1;
+			}
+		
+		}
+		if (aux[i].codigo == 3)
+		{
+			if (aux[i].idade >= 20 && aux[i].idade <= 28)
+			{
+				qnt3 = qnt3 + 1;
+			}
+			
+		}
+	}
+	printf("A quantidade de alunos entre 20 a 28 anos de idade de Engenharia: %d\n",qnt1 );
+	printf("A quantidade de alunos entre 20 a 28 anos de idade de Computação: %d\n",qnt2 );
+	printf("A quantidade de alunos entre 20 a 28 anos de idade de Administração: %d\n",qnt3 );
+
+
+}
