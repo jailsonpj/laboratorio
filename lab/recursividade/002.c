9d64079d45f46cc595d3482fddb6b32591d46e3d
jailsonpj <jailsoncolares@gmail.com>
2016-12-12 10:33:28 -0400

    Update 002.c

--- lab/recursividade/002.c
+++ lab/recursividade/002.c
@@ -10,16 +10,16 @@ int main()
   return 0;
 }
 
-int potencia(int k, int n)
+int potencia(int k, int n) // funcao que calcula a potencia de um numero , onde recebe como paraemtro dois numeros inteiros 
 {
-   int m;
-  if (n == 0)
-    return 1;
-  else if (n % 2 ==0)
+   int m; //inicia uma variavel auxiliar 
+  if (n == 0) // se o expoente for igual a 0
+    return 1; 
+  else if (n % 2 ==0) // se o resto do numero que é o expoente for igual a 0
   {
-    m = potencia (k, n/2);
-    return m*m;
+    m = potencia (k, n/2); // a variavel recebe o valor da chama da funcao recursiva 
+    return m*m; //retorna a varivel multiplicada pela variavel
   }
   else
-    return (k * potencia(k, n-1));
+    return (k * potencia(k, n-1)); //condicao que retorna base * o resultado da funcao decrementando 1
 }
